852e51428f87de8e1dfff83ae35d28c2a8ae211d
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 13:01:57 +0000

    Added VideoDetail, VideoList and VideoListItem

---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="video" class="col-md-8">
+        <div class="ratio ratio-16x9">
+            <iframe :src="videoUrl"/>
+        </div>
+        <div class="details">
+            <h4>{{ video.snippet.title }}</h4>
+            <p>{{ video.snippet.description }}</p>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'VideoDetail',
+        props: ['video'],
+        computed: {
+            videoUrl() {
+                const { videoId } = this.video.id;
+                return `https://www.youtube.com/embed/${videoId}`;
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .details {
+        margin-top: 10px;
+        padding: 10px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list-group col-md-4">
+        <VideoListItem 
+        v-for="video in videos"
+        :key="video.etag"
+        :video="video"
+        @videoSelect="onVideoSelect"
+        >
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+    import VideoListItem from './VideoListItem';
+
+    export default {
+        name: 'VideoList',
+        components: {
+            VideoListItem
+        },
+        // The string in props comes from what you called the property in the parent
+        props: {
+            // Type Validation - recomended
+            videos: Array
+        },
+        methods: {
+            onVideoSelect(video) {
+                this.$emit('videoSelect', video);
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
