6b451a7458e52b3e42007bc68a33e106c4886ba0
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 13:01:08 +0000

    Added SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- @input is equivalent to v-on:input -->
+        <input @input="onInput" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput: function(event) {
+                // The first term of the function is completely up to you. Name the event as you like
+                this.$emit('termChange', event.target.value);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    input {
+        width: 75%;
+    }
+
+    div {
+        text-align: center;
+        margin: 20px;
+    }
+</style>
